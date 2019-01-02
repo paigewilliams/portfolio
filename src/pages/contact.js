36d6cb70e7fbb5e78889e4451b732d1eb2841871
Paige Williams <paw145@humboldt.edu>
2019-01-01 22:40:58 -0800

    Add contact page.

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Email pls</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+  </Layout>
+)
