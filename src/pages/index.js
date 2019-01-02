28283a998a1f7dfc9bd650f3a75a05be52ae2dfa
Paige Williams <paw145@humboldt.edu>
2019-01-01 18:09:36 -0800

    Fix images.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi my name is Paige</h1>
+    <h1>Hi my name</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
