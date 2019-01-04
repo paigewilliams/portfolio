10ac2038da827a9f3dc623557db24536101754a2
Paige Williams <paw145@humboldt.edu>
2019-01-04 09:30:06 -0800

    Populate project pages with body text.

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,31 +23,6 @@ const Sidebar = styled.div`
   background-color: #FFA67A;
   flex: column;
 `
-const StyledTitle = styled.h1`
-
-  letter-spacing: 0.25rem;
-`
-
-// const Sidebar = (props) => (
-  // <div
-  //     style={{
-  //       maxWidth: 960,
-  //       padding: '0.5rem',
-  //       marginBottom: '25px',
-  //       backgroundColor: `#FDA67E`,
-  //       flex: `column`
-  //     }}
-  //     >
-      // <StyledLink to="/"><h2>Paige Williams</h2></StyledLink>
-      // <ul style={{ listStyle: `none` }}>
-      //   <li><ListLink to="/about">About</ListLink></li>
-      //   <li><ListLink to="/projects">Projects</ListLink></li>
-      //   <li><ListLink to="/resume">Resume</ListLink></li>
-      //   <li><ListLink to="/contact/">Contact</ListLink></li>
-      // </ul>
-  // </div>
-  // );
-
 export default ({ children }) => (
   <div style={{ margin: `0.5rem`, display: `flex`, marginLeft: `0.5rem` }}>
     <Sidebar>
