37dd765d2763a84ffd7a3e522233544e7369f777
Paige Williams <paw145@humboldt.edu>
2019-01-04 09:42:34 -0800

    Style project links.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,12 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 import Layout from "../components/layout"
+
+const ProjectLink = styled(Link)`
+  text-decoration: none;
+  color: #2B2B2B;  
+`
 
 export default ({ data }) => {
   return (
@@ -10,10 +16,10 @@ export default ({ data }) => {
       {/* <h4>{data.allMarkdownRemark.totalCount} Projects</h4> */}
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <Link to={node.fields.slug}>
+          <ProjectLink to={node.fields.slug}>
             <h3>{node.frontmatter.title}{" "}</h3>
             <p>{node.excerpt}</p>
-          </Link> 
+          </ProjectLink> 
          </div> 
       ))}
     </div>
