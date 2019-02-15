371826d5d383eb7f0380b1f83514937a8ea8c612
Paige Williams <paw145@humboldt.edu>
2019-02-15 12:05:34 -0800

    Add styling to sidebar component.

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,35 @@
 import React from "react"
+import styled from "styled-components"
+
+const OuterSidebar = styled.div`
+  position: fixed;
+  flex-shrink: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  background-color: #F8C3B2;
+  flex-grow: 1;
+  padding-right: 3rem;
+  padding-left: 1rem;
+  @media (max-width: 1000px) {
+    position: fixed;
+    top: auto;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #F8C3B2;
+    flex-direction: row;
+`
 
  const Sidebar = ({siteTitle}) => (
-  <div>
+  <OuterSidebar>
     <h1>{siteTitle}</h1>
-  </div>
+  </OuterSidebar>
  )
 
  export default Sidebar
