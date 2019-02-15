f465f84fc964c3952b83372ce172b353a74fd4a5
Paige Williams <paw145@humboldt.edu>
2019-02-15 15:42:26 -0800

    Change favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
         background_color: `#FDCDBF`,
         theme_color: `#FDCDBF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/egg.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
