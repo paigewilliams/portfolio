2b484ec6552f90df053e63daf7548f7c04bda80e
Paige Williams <paw145@humboldt.edu>
2019-02-22 10:05:11 -0800

    Refactor Graphql query for projects page; working.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,17 +19,18 @@ const ProjectText = styled.div`
 `
 
 export default ({ data }) => {
+  console.log(data)
   return (
   <Layout>
     <div>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
+      {data.allFile.edges.map(({ node }) => (
         <ProjectsBody key={node.id}>
-          <ProjectLink to={node.fields.slug}>
+          <ProjectLink to={node.childMarkdownRemark.fields.slug}>
             <ProjectText>
-              <h3>{node.frontmatter.title}</h3>
-              <p>{node.excerpt}</p>
+              <h3>{node.childMarkdownRemark.frontmatter.title}</h3>
+              <p>{node.childMarkdownRemark.excerpt}</p>
             </ProjectText>
-            <Img fixed={node.frontmatter.img.childImageSharp.fixed}/>
+            {/* <Img fixed={node.frontmatter.img.childImageSharp.fixed}/> */}
           </ProjectLink> 
          </ProjectsBody> 
       ))}
@@ -40,28 +41,26 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            img {
-              childImageSharp {
-                fixed(width: 400) {
-                  ...GatsbyImageSharpFixed
+    allFile(filter: {relativeDirectory: {regex: "/(pages)/(projects)/"}}) {
+        edges {
+          node {
+            id
+            relativeDirectory
+            childMarkdownRemark {
+              frontmatter {
+                title
+                date
+                img {
+                  id
                 }
               }
+              fields {
+                slug
+              }
+              excerpt
             }
           }
-          fields {
-            slug
-          }
-          excerpt
-        }
+        } 
       }
-    }
   }
 `
--- src/pages/projects/bike-share.md
+++ src/pages/projects/bike-share.md
@@ -2,7 +2,7 @@
 title: "Mock Bikeshare"
 date: "2017-08-21"
 type: "project"
-img: ./projects/bike-share.png
+img: ./../images/bike-share.png
 ---
 
 For the final project in Intro to Programming, four of us created a bike share app, using live data from Biketown. 
