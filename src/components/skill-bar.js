d386a264189b4170236bb835de532ac072a4fa2f
Paige Williams <paw145@humboldt.edu>
2019-02-22 15:18:43 -0800

    Add skills with type and level.

--- src/components/skill-bar.js
+++ src/components/skill-bar.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const skills = [
-  {type: 'front-end', skill: 'JavaScript', level: 75 }
-
-
+  {type: 'front-end', skill: 'JavaScript & TypeScript', level: 75},
+  {type: 'back-end', skill: 'Ruby', level: 50},
+  {type: 'front-end', skill: 'HTML/CSS', level: 85},
+  {type: 'back-end', skill: 'Node.js', level: 30},
+  {type: 'database', skill: 'PostgresSQL', level: 20},
+  {type: 'front-end', skill: 'JavaScript', level: 75},
+  {type: 'database', skill: 'GraphQL', level: 25},
+  {type: 'front-end', skill: 'React/Redux', level: 65},
+  {type: 'database', skill: 'Firebase', level: 40},
+  {type: 'testing', skill: 'Jasmine & Karma', level: 50},
+  {type: 'GIS', skill: 'ArcMap 10.x', level: 80},
+  {type: 'versioning', skill: 'Git', level: 85},
+  {type: 'design', skill: 'Illustrator', level: 65}
 ]
