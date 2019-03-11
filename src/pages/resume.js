a9e058a4bf3b1031fc97e53e29b61c896715dd9f
Paige Williams <paw145@humboldt.edu>
2019-03-10 18:21:24 -0700

    Move experience to top row.

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 import styled from "styled-components"
-// import SkillBar from "../components/skill-bar"
 import VictoryChartData from './../components/victory-bar';
+
 const Header = styled.h1`
-  padding-bottom: 60px;
+  padding-bottom: 1rem;
   top: auto;
   height: 2px;
   width: 100%;
@@ -12,11 +12,14 @@ const Header = styled.h1`
   letter-spacing: 0.10rem;
   line-height: 3rem;
 `
-const ExpEdu = styled.div`
-  display: grid;
-  grid-template-columns: 50% 50%;
-  grid-column-gap: 20px;
-`
+const RowTwo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column; 
+  } 
+` 
 const Body = styled.div`
   display: flex;
   flex-direction: column;
@@ -33,7 +36,7 @@ const SkillsSection = styled.div`
   display: flex;
   flex-direction: column;
   padding-right: 8.3rem;
-  max-width: 50rem;
+  max-width: 80rem;
 `
 const OpenSourceSection = styled.div`
   display: flex;
@@ -70,13 +73,21 @@ export default () => (
           data={skillsLeft}/>
         </SkillsSection>
       <OpenSourceSection>
-        <Header>Open Source Contributions</Header>
-        <h3>Hack Oregon - Cartography and Data Visualization on team Sandox</h3>
-        <p>Feb 2019 - August 2019</p>
+      <Header>Education</Header> 
+      <h3>Certificate in Web Development - Epicodus</h3>
+      <p>Anticipated 2019</p>
+      <ul>
+        <li>
+        Learned Ruby, JavaScript and React through pair-programming. The environment was similar to an office where we work for 40+ hours a week and solve problems as a team. 
+        </li>
+      </ul>
+      <h3>B. A. Environmental Studies with a minor in Geospatial Studies, Cum Laude - Humboldt State University</h3>
+      <p>September 2012 - May 2016</p>
+        
       </OpenSourceSection>
       </RowOne>
-      <ExpEdu>
-        <SkillsSection>
+      <RowTwo>
+      <SkillsSection>
         <Header>Experience</Header>
       <h3>Wine Assistant and Floor Supervisor - Pastaworks</h3>
       <p>March 2017 - January 2019</p>
@@ -103,20 +114,15 @@ export default () => (
       </ul>
         </SkillsSection>
      <OpenSourceSection>
-     <Header>Education</Header> 
-      <h3>Certificate in Web Development - Epicodus</h3>
-      <p>Anticipated 2019</p>
-      <ul>
-        <li>
-        Learned Ruby, JavaScript and React through pair-programming. The environment was similar to an office where we work for 40+ hours a week and solve problems as a team. 
-        </li>
-      </ul>
-      <h3>B. A. Environmental Studies with a minor in Geospatial Studies, Cum Laude - Humboldt State University</h3>
-      <p>September 2012 - May 2016</p>
+     <Header>Open Source Contributions</Header>
+        <h3>Hack Oregon - Cartography and Data Visualization on team Sandox</h3>
+        <p>Feb 2019 - August 2019</p>
   
      </OpenSourceSection>
+      </RowTwo>
+       
       
-      </ExpEdu>
+     
       
       </Body>
   </Layout>
